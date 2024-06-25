057142cf0191460d791e2c68bf99a087b8224e10
Brad <elbeqqal.kamal@gmail.com>
2024-06-25 15:26:49 +0200

    another change

---
+++ string.js
@@ -0,0 +1,8 @@
+// First Section
+
+const btn = document.querySelector("#btn__event");
+const input = document.querySelector(".input__number");
+
+btn.addEventListener("click", () => {
+  console.log(input.value);
+});
