8d53c91369649a868672c0075c83384cde8d36c6
Brad <elbeqqal.kamal@gmail.com>
2024-06-26 16:12:05 +0200

    another change

--- string.js
+++ string.js
@@ -2,21 +2,23 @@
 
 const btn = document.querySelector("#btn__event");
 const input = document.querySelector(".input__number");
-const input_string = document.querySelector(".input__string");
+const input__string = document.querySelector(".input__string");
+const containJs = document.getElementsByClassName("container__js");
 
 // Second Section
 
 function random__generator(string_length) {
   var characters =
     'ABCDEfgehejfhjdhjdjhdjhfdhjf@"ljxkjjxkjxcjxj122slwkxkjxcjkxjcx0';
-  var input__string = "";
+  var input_string = "";
 
   for (var i, i = 0; i < string_length; i++) {
-    input__string += characters.charAt(
+    input_string += characters.charAt(
       Math.floor(Math.random() * characters.length)
     );
   }
-  input_string.innerHTML = characters;
+
+  input__string.innerHTML = input_string;
 }
 
 btn.addEventListener("click", () => {
